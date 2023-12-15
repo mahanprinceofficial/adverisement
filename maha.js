f7a172d212a72a449a538167f86e608ce98b8daf
mahanprinceofficial <149046543+mahanprinceofficial@users.noreply.github.com>
2023-12-16 00:21:46 +0530

    Add files via upload

---
+++ maha.js
@@ -0,0 +1,12 @@
+alert("advertisement");
+let userChoice = confirm("are you watch advertisement");
+if(userChoice)
+{
+    prompt("Enter Your Name "); 
+}
+else{
+    alert("ok no problem")
+}
+
+
+print(alert)
